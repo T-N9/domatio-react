99965f55ca9bb5f483324dffbe70fc524681fbae
T-N9 <thethoriumtension@gmail.com>
2022-10-06 10:38:30 +0630

    [Data] Local Data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, createContext } from "react";
-import LineIcon from 'react-lineicons';
+import LineIcon from "react-lineicons";
 import { MainSection, NavBar, SideBar } from "./components";
+
+/* data */
+import { domatio } from "./data/domatio-data";
 
 /* ==========================================
 Context to deliver category id and tag name 
@@ -8,10 +11,10 @@ to main-section/index.js
 ============================================== */
 const DataContext = createContext();
 export const CategoryTagContext = createContext({
-    categoryId : 0,
-    setCategoryId : () => {},
-    tagName : "",
-    setTagName: () => {}
+  categoryId: 0,
+  setCategoryId: () => {},
+  tagName: "",
+  setTagName: () => {},
 });
 
 /* ==========================================
@@ -19,68 +22,76 @@ LocalStorage initialization for dark theme
 to memorize theme user theme setting in browser
 ============================================== */
 let dark_theme = localStorage.getItem("theme");
-if(dark_theme === null) {
-    localStorage.setItem("theme", false);
-    dark_theme = localStorage.getItem("theme") && false;
-}else if(dark_theme === 'true') {
-    dark_theme = true
-}else if (dark_theme === 'false'){
-    dark_theme = false
+if (dark_theme === null) {
+  localStorage.setItem("theme", false);
+  dark_theme = localStorage.getItem("theme") && false;
+} else if (dark_theme === "true") {
+  dark_theme = true;
+} else if (dark_theme === "false") {
+  dark_theme = false;
 }
 export const ThemeContext = createContext({
-    darkMode : dark_theme,
-    setDarkMode : () => {}
+  darkMode: dark_theme,
+  setDarkMode: () => {},
 });
 
 const App = () => {
-    // State to store data from API
-    const [ dodata, setDoData] = useState([]);
+  // State to store data from API
+  const [dodata, setDoData] = useState([]);
 
-    // States and value to maintain and export context data
-    const [ categoryId, setCategoryId ] = useState(0);
-    const [ tagName, setTagName ] = useState("");
-    const [ darkMode, setDarkMode ] = useState(dark_theme);
-    const categoryTagContextValue = { categoryId, setCategoryId, tagName, setTagName};
-    const themeValue = { darkMode, setDarkMode};
+  // States and value to maintain and export context data
+  const [categoryId, setCategoryId] = useState(0);
+  const [tagName, setTagName] = useState("");
+  const [darkMode, setDarkMode] = useState(dark_theme);
+  const categoryTagContextValue = {
+    categoryId,
+    setCategoryId,
+    tagName,
+    setTagName,
+  };
+  const themeValue = { darkMode, setDarkMode };
 
-    useEffect(() => {
-        fetch('https://t-n9.github.io/domatio-api/domatio-data.json')
-            .then(response => response.json())
-            .then(data => {
-                setDoData(data);
-                console.log({data})
-                // document.querySelector('.loading-content').classList.add('visible-none');
-            });
-    },[])
+  useEffect(() => {
+    // fetch(domatio)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setDoData(data);
+    //         console.log({data})
+    //         // document.querySelector('.loading-content').classList.add('visible-none');
+    //     });
+    setDoData(domatio.domatio);
+  }, []);
 
-    return (
-        <>
-            {/* Data from API context */}
-            <DataContext.Provider value={dodata}>
-                {/* Theme setting layer or wrapper */}
-                <div className={darkMode ? "dark" : ""}>
-                    {/* Main Content */}
-                    <main>
-                        {/* div for scroll to top */}
-                        <span id="up"></span>
-                        <ThemeContext.Provider value={themeValue}>
-                            <NavBar />
-                        </ThemeContext.Provider>
-                            <CategoryTagContext.Provider value={categoryTagContextValue}>
-                                <SideBar/>
-                                <MainSection categoryId={categoryId}/>
-                            </CategoryTagContext.Provider>
-                    </main>
-                    {/* Loading Content before main content */}
-                    <div className="loading-content">
-                        <span className="loading-icon">
-                            <LineIcon name="spinner" />
-                        </span>
-                    </div>
-                </div>
-            </DataContext.Provider>
-        </>
-    )
-}
+  console.log({dodata})
 
-export {App, DataContext};
+  return (
+    <>
+      {/* Data from API context */}
+      <DataContext.Provider value={dodata}>
+        {/* Theme setting layer or wrapper */}
+        <div className={darkMode ? "dark" : ""}>
+          {/* Main Content */}
+          <main>
+            {/* div for scroll to top */}
+            <span id="up"></span>
+            <ThemeContext.Provider value={themeValue}>
+              <NavBar />
+            </ThemeContext.Provider>
+            <CategoryTagContext.Provider value={categoryTagContextValue}>
+              <SideBar />
+              <MainSection categoryId={categoryId} />
+            </CategoryTagContext.Provider>
+          </main>
+          {/* Loading Content before main content */}
+          <div className="loading-content">
+            <span className="loading-icon">
+              <LineIcon name="spinner" />
+            </span>
+          </div>
+        </div>
+      </DataContext.Provider>
+    </>
+  );
+};
+
+export { App, DataContext };
